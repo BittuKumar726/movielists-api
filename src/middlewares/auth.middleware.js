20844c3e66c3eaed90adaff20a58ec205d33b8b4
AdityaRaj140 <17gietucse140@gmail.com>
2024-07-13 12:40:59 +0530

    Updated

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,13 +2,10 @@ import jwt from "jsonwebtoken";
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
-import { request } from "express";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
   try {
-    const token =
-      req.cookies?.accessToken ||
-      req.header("Authorization")?.replace("Bearer ", "");
+    const token = req.header("Authorization")?.replace("Bearer ", "");
 
     if (!token) {
       throw new ApiError(401, "Unauthorized request");
