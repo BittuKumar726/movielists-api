402e404d45822018d1ec4fd1cf5888ebac6bf3f8
AdityaRaj140 <17gietucse140@gmail.com>
2024-07-13 09:29:04 +0530

    Updated

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../uploads/temp");
+    cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
